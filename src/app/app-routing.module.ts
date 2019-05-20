b12646dc57b66271252666f448286260e11077e4
胡方雷 <690328661@qq.com>
2019-05-20 10:56:41 +0800

    123

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'button', loadChildren: './button/button.module#ButtonPageModule' },
+  { path: 'toolbar', loadChildren: './toolbar/toolbar.module#ToolbarPageModule' },
+  { path: 'list', loadChildren: './list/list.module#ListPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
