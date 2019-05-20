2d18dc09d05540ae1a4a538cbe00e5b064d89fd8
胡方雷 <690328661@qq.com>
2019-05-20 10:07:20 +0800

    1234

---
+++ README.md
@@ -0,0 +1,2 @@
+Ionic4.x 中的UI 组件（UI Components）
+ionic4-04
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'button', loadChildren: './button/button.module#ButtonPageModule' },
+  { path: 'toolbar', loadChildren: './toolbar/toolbar.module#ToolbarPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
