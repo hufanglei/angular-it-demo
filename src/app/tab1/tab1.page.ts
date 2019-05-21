479ee75d8f2195a6c47bdae6a685878bb0fa1440
胡方雷 <690328661@qq.com>
2019-05-21 15:42:52 +0800

    init

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,55 @@
+import { Component,ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  @ViewChild('slide1') slide1;
+
+  //轮播图的属性
+  public slidesOpts={
+    speed:400,
+    autoplay: {
+      delay: 2000,
+    },
+    loop:true
+  }
+
+  public slidesList:any[]=[];
+
+
+  //热门商品
+  public hotList:any[]=[];
+
+  public hotListWidth:any=400;
+
+
+  constructor(){
+
+    for(var i=1;i<=3;i++){
+      this.slidesList.push({
+        pic:'assets/slide0'+i+'.png',
+        url:'',
+      })
+    }
+
+    for(var i=1;i<=7;i++){
+      this.hotList.push({
+        pic:'assets/0'+i+'.jpg',
+        title:'第'+i+'个',
+      })
+    }
+
+    //计算hotListWidth的宽度
+    this.hotListWidth=this.hotList.length*9+'rem';
+
+
+  }
+  //手动滑动完成
+  slideTouchEnd(){
+      this.slide1.startAutoplay();
+  }
+}
